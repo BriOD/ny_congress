192b9a787473629d005f8197cf8337cdfbddc613
BriOD <briodonoghue@gmail.com>
2016-08-19 00:38:37 -0400

    playing around and testing

--- lib/ny_congress/cli.rb
+++ lib/ny_congress/cli.rb
@@ -11,15 +11,19 @@ class NyCongress::CLI
 
     #get districts
 
-    puts <<-DOC
-      1         8         15          22
-      2         9         16          23
-      3         10        17          24
-      4         11        18          25
-      5         12        19          26
-      6         13        20          27
-      7         14        21
-      DOC
+    # puts <<-DOC
+    #   1         8         15          22
+    #   2         9         16          23
+    #   3         10        17          24
+    #   4         11        18          25
+    #   5         12        19          26
+    #   6         13        20          27
+    #   7         14        21
+    #   DOC
+
+      #what i ideally want: @districts = NyCongress::District.all
+
+      NyCongress::District.all
   end
 
   def menu
--- lib/ny_congress/district.rb
+++ lib/ny_congress/district.rb
@@ -1,11 +1,19 @@
 class NyCongress::District
   attr_accessor :number, :location, :representative, :contact
 
-@@all = []
+@@all = [  ]
 
 
   def self.all
-    @@all
+    puts <<-DOC
+      1         8         15          22
+      2         9         16          23
+      3         10        17          24
+      4         11        18          25
+      5         12        19          26
+      6         13        20          27
+      7         14        21
+      DOC
   end
 
 
