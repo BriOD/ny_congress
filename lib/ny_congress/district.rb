16aa38b5314ad60057229baafdbcdb4da66ee8b6
BriOD <briodonoghue@gmail.com>
2016-08-22 17:09:35 -0400

    working nokogiri selectors

--- lib/ny_congress/district.rb
+++ lib/ny_congress/district.rb
@@ -14,20 +14,20 @@ class NyCongress::District
     #   6         13        20          27
     #   7         14        21
     #   DOC
-
-      d_1 = self.new
-      d_1.number = "1"
-      d_1.location = "Floral Park"
-      d_1.representative = "Brian OD"
-      d_1.contact = "7187574474"
-
-      d_2 = self.new
-      d_2.number = "2"
-      d_2.location = "Bayside"
-      d_2.representative = "Brian OD"
-      d_2.contact = "7187574474"
-
-[d_1, d_2]
+#
+#       d_1 = self.new
+#       d_1.number = "1"
+#       d_1.location = "Floral Park"
+#       d_1.representative = "Brian OD"
+#       d_1.contact = "7187574474"
+#
+#       d_2 = self.new
+#       d_2.number = "2"
+#       d_2.location = "Bayside"
+#       d_2.representative = "Brian OD"
+#       d_2.contact = "7187574474"
+#
+# [d_1, d_2]
 
   end
 
@@ -41,9 +41,16 @@ class NyCongress::District
   # end
 
   def self.scrape
-    doc = Nokogiri::HTML(open("http://www.house.gov/representatives/#state_ne"))
-
+    page = Nokogiri::HTML(open('http://www.house.gov/representatives/#state_ne'))
+    main_doc = page.xpath('//h2[@id="state_ny"]')
+    doc = main_doc.first.next_element.children.children
+    content = doc.map {|c| c.text.strip.split("\n").reject(&:empty?)}.reject(&:empty?)
+    content.each {|c| c.pop }
     binding.pry
+
+
+
+
 
   end
 
