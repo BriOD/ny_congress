03fc035b5a1a6fad67f69de38a8fb861d0a00b4b
BriOD <briodonoghue@gmail.com>
2016-08-23 16:15:14 -0400

    fixed D to democrat, R to republican issue

--- lib/ny_congress/district.rb
+++ lib/ny_congress/district.rb
@@ -1,71 +1,40 @@
 class NyCongress::District
   attr_accessor :number, :representative, :party, :contact
 
+  @@all = []
+
+
   def initialize(number=nil, representative=nil, party=nil, contact=nil)
     @number = number
     @representative = representative
     @party = party
     @contact = contact
     @@all << self
-
   end
-
-@@all = []
 
 
   def self.all
     @@all
-    # puts <<-DOC
-    #   1         8         15          22
-    #   2         9         16          23
-    #   3         10        17          24
-    #   4         11        18          25
-    #   5         12        19          26
-    #   6         13        20          27
-    #   7         14        21
-    #   DOC
-#
-#       d_1 = self.new
-#       d_1.number = "1"
-#       d_1.location = "Floral Park"
-#       d_1.representative = "Brian OD"
-#       d_1.contact = "7187574474"
-#
-#       d_2 = self.new
-#       d_2.number = "2"
-#       d_2.location = "Bayside"
-#       d_2.representative = "Brian OD"
-#       d_2.contact = "7187574474"
-#
-# [d_1, d_2]
-
   end
 
-  # def self.scrape_districts
-  #   districts = []
-  #
-  #   districts << self.scrape_wiki
-  #
-  #   districts
-  #
-  # end
-
-# http://www.house.gov/representatives/#state_ne
 
   def self.scrape
     page = Nokogiri::HTML(open('http://www.house.gov/representatives/#state_ne'))
     main_doc = page.xpath('//h2[@id="state_ny"]')
     doc = main_doc.first.next_element.children.children
     content = doc.map {|c| c.text.strip.split("\n").reject(&:empty?)}.reject(&:empty?)
-
     useable_content = content[1..28]
     useable_content.each do |d|
+      if d[2] == "D"
+        d[2] = "Democrat"
+      elsif d[2] == "R"
+        d[2] = "Republican"
+      end
+
       NyCongress::District.new(d[0], d[1], d[2], d[4])
+
+
     end
   end
-
-
-
-
 
 end
